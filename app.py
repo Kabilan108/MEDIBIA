8810b5633092ac6add7c8409e693880451bf5dcf
Ebubechukwu Donatus Enwerem <51320069+ENWEREM0DE@users.noreply.github.com>
2023-03-12 03:54:13 -0400

    api codes for disease classifier

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from predictions import classify_skin
+import pickle
+
 
 app = Flask(__name__)
-    
+
+disease_model = pickle.load(open('./notebooks/disease-prediction/my_model_for_healthcare', 'rb'))
 
 @app.route('/skin-condition', methods=['POST'])
 def classify_skin_condition():
@@ -14,6 +17,12 @@ def classify_skin_condition():
     prediction = classify_skin(image)
 
     return prediction, 200
+
+@app.route('/disease_classifier', methods=['POST'])
+def classify_disease():
+    request_data = request.data
+    request_data = json.loads(request_data.decode('utf-8'))
+    userMessage = request_data['message'] 
 
 
 @app.route('/')
