894c0bda7dfad6af8221171f940eb943f657d175
Tony Okeke <tonykabilanokeke@gmail.com>
2023-03-12 01:35:05 -0500

    Remove extra endpoints

--- app.py
+++ app.py
@@ -2,46 +2,6 @@ from flask import Flask, request, jsonify
 from predictions import classify_skin
 
 app = Flask(__name__)
-
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from the url parameter /getmsg/?name=
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"Received: {name}")
-
-    response = {}
-
-    # Check if the user sent a name at all
-    if not name:
-        response["ERROR"] = "No name found. Please send a name."
-    # Check if the user entered a number
-    elif str(name).isdigit():
-        response["ERROR"] = "The name can't be numeric. Please send a string."
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome API!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "No name found. Please send a name."
-        })
     
 
 @app.route('/skin-condition', methods=['POST'])
@@ -58,7 +18,7 @@ def classify_skin_condition():
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to MEDIBIA!</h1>"
+    return "<h4>Welcome to MEDIBIA!</h4>"
 
 
 if __name__ == '__main__':
